537fe31c3c22ca3a86a6ea1273db5156a255ff4c
Diego <ruizd2218@gmail.com>
2022-02-27 14:33:24 -0800

    Created short welcome page

---
+++ Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace myConsoleProject.cs
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" for you!");
+            Console.Read();
+        }
+    }
+}
