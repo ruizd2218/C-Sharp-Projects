470fd299804020c1e9a76f23920d66216199078a
Diego <ruizd2218@gmail.com>
2022-02-26 16:28:02 -0800

    Created my first C# console App

---
+++ Basic_C#_Programs/SampleApplication.cs
@@ -0,0 +1,9 @@
+using System;
+class SampleApplication {
+    // Main() is a function that tells the compiler to execute anything between 
+    // the curly braces. In a console application, a function with the name 
+    // “Main()” is automatically executed as the first step in running the program
+    static void Main() {
+        Console.WriteLine("Hello, World");
+    }
+}
