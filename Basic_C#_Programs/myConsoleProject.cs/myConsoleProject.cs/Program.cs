f47a0c7fc159180b0f842bcc872d0296490a306c
Diego <ruizd2218@gmail.com>
2022-02-28 04:03:38 -0800

    basic program assignment

--- Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
+++ Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -6,9 +6,23 @@ namespace myConsoleProject.cs
     class Program
     {
         static void Main()
-        {
-            Console.WriteLine("Welcome to Acme Accounting Systems");
-            Console.WriteLine("Remember, we're \"accounting\" for you!");
+        {            
+            Console.WriteLine("The Tech Academy."); //writes basic lines to console
+            Console.WriteLine("Student Daily Report.");
+            Console.WriteLine("What is your name?"); //writes the first line with the initial question we have to the user
+            string name = Console.ReadLine(); //sets the input from the user to string variable "name"
+            Console.WriteLine("What course are you currently on?");
+            string course = Console.ReadLine();
+            Console.WriteLine("What page number are you on?");
+            int pageNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Do you need any help? (True, False)");
+            bool help = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("Any positive experiences you would like to mention?");
+            string exp = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            //writes this last line to show we are now finished taking input.
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.Read();
         }
     }
