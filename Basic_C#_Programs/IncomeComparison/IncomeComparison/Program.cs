c93e0f48e75bdb6dd42fce318ab78fd110868e99
Diego <ruizd2218@gmail.com>
2022-02-28 17:18:08 -0800

    created basic income comparison program

---
+++ Basic_C#_Programs/IncomeComparison/IncomeComparison/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IncomeComparison
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            Console.WriteLine("Person 1");
+            Console.WriteLine("What is this person's hourly rate?");
+            float rate1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("How many hours does this person work per week?");
+            float hours1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Person 2");
+            Console.WriteLine("What is this person's hourly rate?");
+            float rate2 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("How many hours does this person work per week?");
+            float hours2 = Convert.ToSingle(Console.ReadLine());
+
+            float Income1 = rate1 * hours1 * 50;
+            float Income2 = rate2 * hours2 * 50;
+            Console.WriteLine("Annual salary of person 1 is - " + Income1);
+            Console.WriteLine("Annual salary of person 2 is - " + Income2);
+            bool IncomeComp = Income1 >= Income2;
+            Console.WriteLine("Does person 1 make more money than person two? - " + IncomeComp);
+            Console.Read();
+        }
+    }
+}
---
+++ Basic_C#_Programs/TestEnv/TestEnv/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace TestEnv
+{
+    class Program
+    {
+        static void Main()
+        {
+            //bool trueOrFalse = 12 > 5;
+            //.ToString() converts the boolean value to a string
+            //Console.Write(trueOrFalse.ToString()); 
+            //Console.Read();
+
+            int roomTemperature = 70;
+            int currentTemperature = 72;
+            bool isWarm = currentTemperature > roomTemperature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+        }
+    }
+}
