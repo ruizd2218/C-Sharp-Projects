037391145440870e1a98adee9b6cb8bf5db875c6
Diego <ruizd2218@gmail.com>
2022-03-02 05:33:45 -0800

    created package shipping application

---
+++ Basic_C#_Programs/PackageExpress/PackageExpress/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace PackageExpress
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express! Please follow the instructions below.");
+            Console.WriteLine("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+            Console.WriteLine("Please enter the package weight.");
+            int weight = Convert.ToInt32(Console.ReadLine());
+            if (weight > 50)
+            {
+                Console.WriteLine("Package too heavy. Press enter to exit application. ");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else
+            {}
+
+            Console.WriteLine("Please enter the package WIDTH");
+            int w = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package HEIGHT");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the package LENGTH");
+            int l = Convert.ToInt32(Console.ReadLine());
+
+            int dimAdd = w + h + l;
+            if (dimAdd > 50)
+            {
+                Console.WriteLine("Package too large. Press enter to exit application. ");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else 
+            {}
+
+            int dimX = w * h * l;
+            int dimFinal = dimX * weight; 
+            int quote = dimFinal / 100;
+            Console.WriteLine("Your estimated total for shipping this package is: " + quote.ToString("C"));
+            Console.ReadLine();
+        }
+    }
+}
