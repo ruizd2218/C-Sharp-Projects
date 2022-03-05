be4561c7a5fbd29ece1161c763d8e78d4bf2904f
Diego <ruizd2218@gmail.com>
2022-03-05 14:34:44 -0800

    assignment

---
+++ Basic_C#_Programs/Methods/Methods/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Math
+    {
+        public static int Addition(int num1)
+        {
+            int result = num1 + 600;
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static int Subtraction(int num2)
+        {
+            int result = num2 - 20;
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static int Multiplication(int num3)
+        {
+            int result = num3 * 15;
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
---
+++ Basic_C#_Programs/Methods/Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to add by 600.");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Math.Addition(input1);
+
+            Console.WriteLine("Enter a number to subtract by 20");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            Math.Subtraction(input2);
+
+            Console.WriteLine("Enter a number to multiply by 15");
+            int input3 = Convert.ToInt32(Console.ReadLine());
+            Math.Multiplication(input3);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
