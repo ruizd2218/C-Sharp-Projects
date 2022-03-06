092b5bb0a3d449931a169be6fc20c9846c2bdb9c
Diego <ruizd2218@gmail.com>
2022-03-05 18:49:01 -0800

    assignment

---
+++ Basic_C#_Programs/Method2/Method2/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Math
+    {
+        public static int Operation(int num1, int num2 = 65)
+        {
+            int num3 = num1 + num2;
+            Console.WriteLine(num3);
+            return num3;
+        }
+    }
+}
---
+++ Basic_C#_Programs/Method2/Method2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input1 = 0;
+            int input2 = 0;
+            try
+            {
+                Console.WriteLine("Input atleast one operator to multiply");
+                Console.WriteLine("First Number-");
+                input1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Second Number (you don't need to enter anything here)-");
+                input2 = Convert.ToInt32(Console.ReadLine());
+
+                
+            }
+            catch (System.FormatException)
+            {
+             
+                Math.Operation(input1);
+                Console.Read();
+            }
+
+
+
+        }
+    }
+}
