c22a98e86507385391f8bfe4c9bfb05c7fb83dc8
Diego <ruizd2218@gmail.com>
2022-03-07 20:20:44 -0800

    Assignment

---
+++ Basic_C#_Programs/ClassAssignment285/ClassAssignment285/Divide.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment285
+{
+    class Divide
+    {
+        public void DivideBy2(int n1)
+        {
+            int answer = n1 / 2;
+            Console.WriteLine(answer);
+        }
+
+        public int DivideALot(int n2, out int times)
+        {
+            //this will divide by 2, a random amount of times
+            Random rnd = new Random();
+
+
+            times = rnd.Next(2, 10); //picks a random number between 2 and 10
+            int newNum = n2;
+            for (int i = 0; i < times; i++) //this will run only the amount of times specified by the RNG
+            {
+                newNum = newNum / 2; //divides our number by 2
+            }
+
+            return newNum;
+
+        }
+
+        public static int DivideALot(int n3)
+        {
+            //pretty much the same but divides only 5 times
+            int newNum = n3;
+            for (int i = 0; i < 5; i++) 
+            {
+                newNum = newNum / 2;
+            }
+
+            return newNum;
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClassAssignment285/ClassAssignment285/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment285
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number - ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            Divide result = new Divide(); //creating an instance of the Divide class
+            result.DivideBy2(input); 
+
+            int times = 0; //declaring variable so we can use it
+            Console.WriteLine("Enter a number - ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            int answer = result.DivideALot(input2, out times);
+
+            Console.WriteLine(answer); //displays answer
+            Console.WriteLine("Your number was divided " + times + " times."); //displays how many times number was divided
+
+            int answer2 = Divide.DivideALot(n3: 160);
+            Console.WriteLine(answer2);
+
+            Console.Read();
+        }
+
+    }
+}
