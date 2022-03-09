7fd735374987845ed2d8d127a65686b92d3b1104
Diego <ruizd2218@gmail.com>
2022-03-08 17:58:32 -0800

    assignment

---
+++ Basic_C#_Programs/AbstractClass302/AbstractClass302/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass302
+{
+    abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public void sayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+
+
+    }
+}
