b2c651323d924a95dd56a74b472ce31848d29559
Diego <ruizd2218@gmail.com>
2022-03-08 16:57:05 -0800

    assignment

---
+++ Basic_C#_Programs/MethodsAndObjects292/MethodsAndObjects292/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects292
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Employee emp = new Employee();
+
+            emp.FirstName = "Sample";
+            emp.LastName = "Student";
+            emp.SayName();
+            
+            Console.Read();
+
+        }
+    }
+}
