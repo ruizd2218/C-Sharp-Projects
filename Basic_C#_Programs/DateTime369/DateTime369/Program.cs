fe9d4a0d382ad5f38ebe6b43b1e4926e72d59c36
Diego <ruizd2218@gmail.com>
2022-03-11 06:45:27 -0800

    assignment

---
+++ Basic_C#_Programs/DateTime369/DateTime369/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime369
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime timeNow = DateTime.Now; //gets the time right now and is saved as timeNow
+            Console.WriteLine(timeNow);
+            
+            Console.WriteLine("enter a number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            DateTime newTime = timeNow.AddHours(input); //adds the number that user inputs to the hours of the current time
+            Console.WriteLine("in {0} hours, it will be {1}", input, newTime); //prints new time
+            Console.ReadLine();
+
+        }
+    }
+}
--- Basic_C#_Programs/TwentyOne/TwentyOne/Card.cs
+++ Basic_C#_Programs/TwentyOne/TwentyOne/Card.cs
@@ -6,9 +6,37 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    public class Card
+    public struct Card
+    { 
+        public Suit Suit { get; set; }
+        public Face Face { get; set; }
+        
+        public override string ToString()
+        {
+            return string.Format("{0} of {1}", Face, Suit);
+        }
+    }
+    public enum Suit
     {
-        public string Suit { get; set; }
-        public string Face { get; set; }
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+    public enum Face
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
     }
 }
