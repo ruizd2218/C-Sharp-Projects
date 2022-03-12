566466cf084cfea948e9fde75af4c0941cc6d308
Diego <ruizd2218@gmail.com>
2022-03-12 15:16:26 -0800

    assignment

---
+++ Basic_C#_Programs/Const383/Const383/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const383
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string avocadoes = "Avocadoes are edible";
+            var apples = "apples are edible";
+
+            System.Console.WriteLine(avocadoes +"\n"+ apples);
+            System.Console.Read();
+        }
+    }
+}
