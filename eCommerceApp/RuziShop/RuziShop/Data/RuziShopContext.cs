d5774ce474146c9e889db1c4a684128d8b5e22b8
Diego Ruiz <95021801+ruizd2218@users.noreply.github.com>
2022-05-01 13:41:46 -0700

    created main page and layout page

---
+++ eCommerceApp/RuziShop/RuziShop/Data/RuziShopContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RuziShop.Data
+{
+    public class RuziShopContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public RuziShopContext() : base("name=RuziShopContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<RuziShop.Models.Product> Products { get; set; }
+    }
+}
---
+++ eCommerceApp/RuziShop/RuziShop/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RuziShop.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Bio { get; set; }
+        public decimal Price { get; set; }
+        public byte[] Photo { get; set; }
+    }
+}
